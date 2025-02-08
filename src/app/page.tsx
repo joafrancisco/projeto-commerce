fcc4de2f952e50d8a61870c107cdc17aee8e3527
ghost <franciscoking125@gmail.com>
2025-02-08 13:42:57 -0300

    Listagem de Produtos Parte 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
-// import Image from "next/image";
-
 export default function Home() {
   return (
-    <div>
+    <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+          prod 1
+        </div>
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+          prod 1
+        </div>
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+          prod 1
+        </div>
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+          prod 1
+        </div>
+      </div>
       <h1>Projeto-Commerce</h1>
     </div>
   );
